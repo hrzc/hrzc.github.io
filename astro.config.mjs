333a20dae3c5474595ea5394c4f154f72f4f2c5e
hrzc <172122364+hrzc@users.noreply.github.com>
2024-06-09 12:08:09 -0400

    20240609T160809Z

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://hrzc.github.io",
+  site: "https://hrzc.github.io",
+  integrations: [tailwind()]
 });
